d3eda5d69dd4abaeff70396a9f2ea5bbbb86f4f9
Gilsabo <gilsala_88@hotmail.com>
2023-09-06 15:22:56 +0200

    Fetch website content

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs';
+import fetch from 'node-fetch';
 
 fs.mkdir('./memes', function (err) {
   if (err) {
@@ -7,3 +8,10 @@ fs.mkdir('./memes', function (err) {
     console.log('New directory successfully created.');
   }
 });
+
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+
+console.log(body);
