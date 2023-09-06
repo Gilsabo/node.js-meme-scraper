6f3e04c9eabb7ce36b9407f0b150fabdf1c44781
Gilsabo <gilsala_88@hotmail.com>
2023-09-06 17:18:50 +0200

    Create an array to store all 10 urls of the images

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-import fs from 'node:fs';
+// import fs from 'node:fs';
+import * as cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
-fs.mkdir('./memes', function (err) {
+/* fs.mkdir('./memes', function (err) {
   if (err) {
     console.log(err);
   } else {
     console.log('New directory successfully created.');
   }
-});
+}); */
 
 const response = await fetch(
   'https://memegen-link-examples-upleveled.netlify.app/',
 );
 const body = await response.text();
 
-console.log(body);
+const $ = cheerio.load(body);
+
+const listItems = $('a').find('img');
+
+console.log(listItems[0].attribs.src);
+
+const arrayImages = [];
+
+for (let i = 0; i <= 9; i++) {
+  arrayImages.push(listItems[i].attribs.src);
+  console.log(i);
+}
+
+console.log(arrayImages);
